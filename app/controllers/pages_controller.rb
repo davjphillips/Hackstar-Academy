3cae2022abd9e628c2e3a692074bc2998c4dc37b
David J. Phillips <D@sutterstudios.com>
2012-04-23 17:40:19 -0700

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def testimonials
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackstarAcademy::Application.config.secret_token = '2d4f818aa4185282230378295485e2569dda0c53930a6e567848b57543231ae9184bc7a1043c7680af3a5e304d27dc96c14425c865a4c2a104f6e6e4338ec749'
